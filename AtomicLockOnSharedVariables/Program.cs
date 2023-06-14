7225d03707af4cf9c657e6cc5c0349ec561aeabe
Rahma_salah <58772969+Rahma-salah44@users.noreply.github.com>
2023-06-14 03:21:20 +0200

    push

---
+++ AtomicLockOnSharedVariables/Program.cs
@@ -0,0 +1,16 @@
+﻿int counter = 0;
+
+void Increase()
+{
+    for (int i = 0; i < 1000000; i++)
+    {
+        Interlocked.Increment(ref counter);
+
+    }
+    Console.WriteLine("The counter is " + counter);
+}
+
+Task.Run(() => Increase());
+Task.Run(() => Increase());
+
+Console.ReadLine();
